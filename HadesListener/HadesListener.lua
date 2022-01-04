342d245a005c7946d1acbb9ead3493ebc7e689a1
Andre Issa <andrelouisissa@gmail.com>
2022-01-04 16:17:09 +1100

    doesn't crash on startup

--- HadesListener/HadesListener.lua
+++ HadesListener/HadesListener.lua
@@ -2,7 +2,10 @@ ModUtil.Mod.Register("HadesListener")
 
 local hooks = { }
 
-function HadesListener.AddHook( prefix, hook )
+function HadesListener.AddHook( hook, prefix )
+	if prefix == nil then
+		prefix = ""
+	end
 	if type( prefix ) ~= "string" then
 		prefix = ModUtil.Identifiers.Inverse[ prefix ]
 	end
@@ -11,49 +14,50 @@ function HadesListener.AddHook( prefix, hook )
 	hooks[ prefix ] = base
 end
 
-local function poll( ... )
+local function poll( )
 
-  local valid, msg = pcall( load )
-  if not valid then return print( msg ) end
-  local threaded, delay, file = false, true
-  local thread, wait = thread, wait
-  local loadfile, pcall, setfenv = loadfile, pcall, setfenv
-  local loadOnce, debugCall = ModUtil.LoadOnce, ModUtil.DebugCall
+	local valid, msg = pcall( load )
+	if not valid then return print( msg ) end
+	local threaded, delay, file = false, true
+	local thread, wait = thread, wait
+	local loadfile, pcall, setfenv = loadfile, pcall, setfenv
+	local loadOnce, debugCall = ModUtil.LoadOnce, ModUtil.DebugCall
+	
+	local wpoll = function( )
+		while threaded do
+			pcall( wait, delay )
+			if not threaded then return end
+			epoll( )
+		end
+	end
+	local bpoll
+	local epoll = function( ) 
+		if not valid and file then
+			valid, msg = pcall( loadfile, file )
+		end
+		return bpoll( )
+	end
+	bpoll = function( )
+		if valid then
+			setfenv( msg, _ENV )
+			file, delay = debugCall( msg, hooks )
+			valid = false
+		end
+		if not delay then
+			threaded = false
+			return epoll( )
+		end
+		if delay == true then
+			threaded = false
+			return loadOnce( epoll )
+		end
+		if not threaded then
+			threaded = true
+			return thread( wpoll )
+		end
+	end
 
-  local function wpoll( )
-    while threaded do
-      pcall( wait, delay )
-      if not threaded return end
-      epoll( )
-    end
-  end
-  local bpoll
-  local function epoll( ) 
-    if not valid and file then
-      valid, msg = pcall( loadfile, file )
-    end
-    return bpoll( )
-  end
-  function bpoll( )
-    if valid then
-      setfenv( msg, _ENV )
-      file, delay = debugCall( msg, ... )
-      valid = false
-    end
-    if not delay then
-      threaded = false
-      return epoll( )
-    end
-    if delay == true then
-      threaded = false
-      return loadOnce( epoll )
-    end
-    if not threaded then
-      threaded = true
-      return thread( wpoll )
-    end
-  end
-
-  return bpoll( )
+	return bpoll( )
 end
-return poll( hooks )
+
+return poll( )
