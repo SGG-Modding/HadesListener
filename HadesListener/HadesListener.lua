afeee048af72e2063f0ddf08cd66f1ffd07ee4fe
Andre Issa <andrelouisissa@gmail.com>
2022-01-05 22:09:07 +1100

    obsessively oust all heap allocations in the passive polling process

--- HadesListener/HadesListener.lua
+++ HadesListener/HadesListener.lua
@@ -1,10 +1,11 @@
 ModUtil.Mod.Register("HadesListener")
 
 local hooks = { }
+local senders = { }
 local delay = 0.05
-local print = ModUtil.Print
-local debugCall = ModUtil.DebugCall
 local notify
+local debugCall = ModUtil.DebugCall
+local print, pcall, loadfile, select, rawipairs, rawpairs, yield = print, pcall, loadfile, select, rawipairs or ipairs, rawpairs or pairs, coroutine.yield
 
 print( "HadesListener: Lua Refreshed!" )
 
@@ -17,24 +18,26 @@ local function startswith( s, p )
 	return s:sub( i )
 end
 
-local function poll( )
-	local valid, msg = false
-	local file = "proxy_stdin.txt"
-	while true do
-		if not valid then
-			--print( "HadesListener: Polling...", file )
-			valid, msg = debugCall( loadfile, file )
+--http://lua-users.org/wiki/VarargTheSecondClassCitizen
+local vararg
+do
+	local i, t, l = 0, {}
+	local function iter(...)
+		i = i + 1
+		if i > l then return end
+		return i, t[i]
+	end
+
+	function vararg(...)
+		i = 0
+		l = select("#", ...)
+		for n = 1, l do
+			t[n] = select(n, ...)
 		end
-		if valid and msg then
-			valid = false
-			setfenv( msg, _ENV )
-			local out  = table.pack( debugCall( msg ) )
-			file = table.unpack( out, 2, 2 )
-			if out.n >= 3 then
-				HadesListener.Notify( table.unpack( out, 3, out.n ) )
-			end
+		for n = l+1, #t do
+			t[n] = nil
 		end
-		debugCall( waitScreenTime, delay )
+		return iter
 	end
 end
 
@@ -48,16 +51,17 @@ function HadesListener.AddHook( hook, prefix )
 	local base = hooks[ prefix ] or { }
 	table.insert( base, hook )
 	hooks[ prefix ] = base
+	senders[ prefix ] = function( s ) return print( prefix .. s ) end
 end
 
 function HadesListener.Notify( ... )
-	for _, message in ipairs( { ... } ) do
+	for _, message in vararg( ... ) do
 		--print( "HadesListener: Received: " .. message )
-		for prefix, callbacks in pairs( hooks ) do
+		for prefix, callbacks in rawpairs( hooks ) do
 			local tail = startswith( message, prefix )
 			if tail then
-				for _, callback in ipairs( callbacks ) do
-					debugCall(callback, tail, function( s ) return print( prefix .. s ) end )
+				for _, callback in rawipairs( callbacks ) do
+					debugCall( callback, tail, senders[ prefix ] )
 				end
 			end
 		end
@@ -65,5 +69,28 @@ function HadesListener.Notify( ... )
 end
 
 notify = HadesListener.Notify
-thread( poll )
 
+do
+	local waitArgs = setmetatable({ screenTime = true }, {
+		__index = function( _, k )
+			if k == "wait" then return delay end
+			if k == "threadInfo" then return lastGoodThreadInfo end
+			return nil
+		end
+	})
+	
+	local function handle( valid, file, ... )
+		if valid then notify( ... ) end
+		return file
+	end
+
+	local function poll( )
+		local file = "proxy_stdin.txt"
+		while true do
+			--print( "HadesListener: Polling...", file )
+			file = handle( pcall( dofile, file ) )
+			yield( waitArgs )
+		end
+	end
+	thread( poll )
+end
