7281db87e9ded9e47b568051f3b3b31f759bc15c
Andre Issa <andrelouisissa@gmail.com>
2022-01-14 02:33:37 +1100

    local environment for lua REPL

--- Content/Mods/StyxScribeREPL/StyxScribeREPL.lua
+++ Content/Mods/StyxScribeREPL/StyxScribeREPL.lua
@@ -1,11 +1,28 @@
 ModUtil.Mod.Register( "StyxScribeREPL" )
 
+StyxScribeREPL.Globals = { }
+StyxScribeREPL.Environment = setmetatable( { }, {
+	__index = function( _, k )
+		local v = _ENV[ k ]
+		if v ~= nil then return v end
+		return StyxScribeREPL.Globals[ k ]
+	end,
+	__newindex = function( _, k, v )
+		if _ENV[ k ] ~= nil then
+			_ENV[ k ] = v
+			return
+		end
+		StyxScribeREPL.Globals[ k ] = v
+	end
+} )
+
 function StyxScribeREPL.RunLua( message )
 	local func, err = load( "return " .. message )
 	if not func then
 		func, err = load( message )
 		if not func then return print( err ) end
 	end
+	setfenv( func, StyxScribeREPL.Environment )
 	local ret = table.pack( pcall( func ) )
 	if ret.n <= 1 then return end
 	return ModUtil.Print( ModUtil.Args.Map( ModUtil.ToString.Value, table.unpack( ret, 2, ret.n ) ) )
